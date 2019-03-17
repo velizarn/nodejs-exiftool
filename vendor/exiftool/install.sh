4ace7e20daa23bf61e83c7c8dee00c7dfe5c434a
Velizar Nenov <vndepot-wdg@yahoo.com>
2019-03-17 21:04:24 +0200

    Update install.sh

--- vendor/exiftool/install.sh
+++ vendor/exiftool/install.sh
@@ -4,5 +4,5 @@ gzip -dc Image-ExifTool-11.32.tar.gz | tar -xf -
 cd Image-ExifTool-11.32
 dir=$(pwd)
 perl Makefile.PL
-make test
+# make test
 echo "export EXIFTOOL_PATH=$dir/exiftool" > $HOME/.profile
